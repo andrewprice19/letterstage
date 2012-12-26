2597637a6923baddbfceb01790640ea2aea17d53
Robert Curth <robert@rocu.de>
2012-12-26 16:25:19 +0100

    Removed spec

--- spec/image_converter_spec.rb
+++
@@ -1,9 +0,0 @@
-#jp2a --width=50 --height=30 --grayscale -b
-
-
-describe ImageConverter do
-  it 'converts a image' do
-    converter = ImageConverter.new('/Users/shostakovich/face.jpg')
-    converted_image = converter.convert(50, 50).length == 2500
-  end
-end
